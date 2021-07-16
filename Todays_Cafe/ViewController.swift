9fe6aa61c1f9a3e8a77298ded7437723e8aedb67
Minny27 <dltmdals0608@naver.com>
2021-07-16 15:32:41 +0900

    Initial Commit

---
+++ Todays_Cafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todays_Cafe
+//
+//  Created by SeungMin on 2021/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
