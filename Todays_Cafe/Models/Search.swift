e01d9c37017dca46255ff9aa330e1b9eeb30fcad
Minny27 <dltmdals0608@naver.com>
2021-07-28 16:51:46 +0900

    Feat: Search 모델 생성 - bannerText, bannerImageName 변수 생성

---
+++ Todays_Cafe/Models/Search.swift
@@ -0,0 +1,16 @@
+//
+//  cellInfo.swift
+//  Todays_Cafe
+//
+//  Created by SeungMin on 2021/07/27.
+//
+
+import Foundation
+
+struct Search {
+    let searchText: String
+    
+    init(_ searchText: String) {
+        self.searchText = searchText
+    }
+}
