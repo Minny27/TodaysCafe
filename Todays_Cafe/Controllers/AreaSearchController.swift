2dbb847d63af10708f48c8d5af0cf27b6131526b
Minny27 <dltmdals0608@naver.com>
2021-07-31 23:37:11 +0900

    Feat: AreaSearchController 생성 - 뒤로 가기 버튼 및 뒤로 가기 이벤트 생성

---
+++ Todays_Cafe/Controllers/AreaSearchController.swift
@@ -0,0 +1,23 @@
+//
+//  areaSearchController.swift
+//  Todays_Cafe
+//
+//  Created by SeungMin on 2021/07/31.
+//
+
+import UIKit
+
+class AreaSearchController: UIViewController {
+    @IBOutlet weak var backBtn: UIButton! {
+        didSet {
+            self.backBtn.setImage(UIImage(systemName: "arrow.backward"), for: .normal)
+            self.backBtn.contentHorizontalAlignment = .fill
+            self.backBtn.contentVerticalAlignment = .fill
+            self.backBtn.tintColor = .darkGray
+        }
+    }
+    
+    @IBAction func onClickBackBtn(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+}
