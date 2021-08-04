ec7bc0066fb9d3760bec8d550f86352b3e718c04
Minny27 <dltmdals0608@naver.com>
2021-08-04 18:36:00 +0900

    Feat: 컬렉션 뷰 안 나오는 문제 해결, 이름 변경 - UICollectionViewDataSource 프로토콜 메서드 재정의 - viewDidLoad()에서 컬렉션 뷰 데이터 소스 할당 - TagSearchController -> TagSearchViewController - onRecommendTagBtnClicked, onRecentSearchTagBtnClicked 버튼 이벤트 생성

---
+++ Todays_Cafe/Controllers/TagSearchViewController.swift
@@ -0,0 +1,73 @@
+//
+//  SearchController.swift
+//  Todays_Cafe
+//
+//  Created by SeungMin on 2021/07/31.
+//
+
+import UIKit
+
+class TagSearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    let tagViewModel2 = TagViewModel2()
+    
+    @IBOutlet weak var tagCollectionView2: UICollectionView!
+    
+    @IBOutlet weak var backBtn: UIButton! {
+        didSet {
+            self.backBtn.setImage(UIImage(systemName: "arrow.backward"), for: .normal)
+            self.backBtn.contentHorizontalAlignment = .fill
+            self.backBtn.contentVerticalAlignment = .fill
+            self.backBtn.tintColor = .darkGray
+        }
+    }
+    
+    
+    @IBOutlet weak var recommendTagBtn: UIButton! {
+        didSet {
+            self.recommendTagBtn.setTitle("추천 테마", for: .normal)
+            self.recommendTagBtn.setTitleColor(.gray, for: .normal)
+            self.recommendTagBtn.titleLabel?.font = .boldSystemFont(ofSize: 20)
+            self.recommendTagBtn.contentHorizontalAlignment = .right
+        }
+    }
+    
+    @IBOutlet weak var recentSearchTagBtn: UIButton! {
+        didSet {
+            self.recentSearchTagBtn.setTitle("최근 검색 테마", for: .normal)
+            self.recentSearchTagBtn.setTitleColor(.gray, for: .normal)
+            self.recentSearchTagBtn.titleLabel?.font = .boldSystemFont(ofSize: 20)
+            self.recentSearchTagBtn.contentHorizontalAlignment = .left
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tagCollectionView2.delegate = self
+        self.tagCollectionView2.dataSource = self
+    }
+    
+    @IBAction func onBackBtnClicked(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onRecommendTagBtnClicked(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func onRecentSearchTagBtnClicked(_ sender: Any) {
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tagViewModel2.countTagList
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "tagCollectionViewCell2", for: indexPath) as? TagCollectionViewCell2 else { return UICollectionViewCell()
+        }
+        let cellInfo = tagViewModel2.tagInfo(at: indexPath.item)
+        cell.update(tagInfo: cellInfo)
+        return cell
+    }
+
+}
