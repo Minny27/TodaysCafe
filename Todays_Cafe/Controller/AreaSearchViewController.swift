f7c0283d8c7a7fcf74c4b851729971788bdb8606
Minny27 <dltmdals0608@naver.com>
2021-08-31 23:01:14 +0900

    Feat: 시 별 구의 정보를 담는 탭바 생성, DataSource, Delegate 변경 - 탭바의 정보를 담는 cityTabbarViewModel 객체 생성 - 각 시, 도의 구 정보를 가지는 각각의 뷰모델 객체 생성 - 각 시를 클릭했을 때 클릭을 표시하는 checkCityHashTable 생성 - cityTabbarCollecionView 객체 생성 - 각 컬렉션뷰를 조건문으로 데이터를 할당 - 탭바의 셀을 클릭할 경우, 체크 상태 확인 후 리로드 - 구 컬렉션뷰 셀을 클릭할 경우, cityName 갱신 및 areaInfo 초기화 - cityName, areaInfo를 CafeBlogTableVC에 전달

--- Todays_Cafe/Controller/AreaSearchViewController.swift
+++ Todays_Cafe/Controller/AreaSearchViewController.swift
@@ -8,42 +8,21 @@
 import UIKit
 
 class AreaSearchViewController: UIViewController {
-    let areaViewModel = AreaViewModel()
-    let areaCollectionViewCell = AreaCollectionViewCell()
+    let cityTabbarViewModel = CityTabbarViewModel()
+    var seoulAreaViewModel = SeoulAreaViewModel()
+    var gyeonggiAreaViewModel = GyeonggiAreaViewModel()
+    var incheonAreaViewModel = IncheonAreaViewModel()
     var tagName: String?
+    var cityNmae: String = ""
+    var checkCityHashTable: [String: Int] = ["seoul": 1, "gyeonggi": 0, "incheon": 0]
+    
+    @IBOutlet weak var cityTabbarCollecionView: UICollectionView!
     
     @IBOutlet weak var areaCollectionView: UICollectionView! {
         didSet {
             self.areaCollectionView.layer.borderWidth = 1.5
             self.areaCollectionView.layer.borderColor = UIColor.lightGray.cgColor
             self.areaCollectionView.layer.cornerRadius = 10
-        }
-    }
-    
-    @IBOutlet weak var seoulBtn: UIButton! {
-        didSet {
-            self.seoulBtn.setTitle("서울", for: .normal)
-            self.seoulBtn.setTitleColor(.gray, for: .normal)
-            self.seoulBtn.titleLabel?.font = .boldSystemFont(ofSize: 20)
-            self.seoulBtn.contentHorizontalAlignment = .center
-        }
-    }
-    
-    @IBOutlet weak var gyeonggiBtn: UIButton! {
-        didSet {
-            self.gyeonggiBtn.setTitle("경기", for: .normal)
-            self.gyeonggiBtn.setTitleColor(.gray, for: .normal)
-            self.gyeonggiBtn.titleLabel?.font = .boldSystemFont(ofSize: 20)
-            self.gyeonggiBtn.contentHorizontalAlignment = .center
-        }
-    }
-    
-    @IBOutlet weak var inchonBtn: UIButton! {
-        didSet {
-            self.inchonBtn.setTitle("인천", for: .normal)
-            self.inchonBtn.setTitleColor(.gray, for: .normal)
-            self.inchonBtn.titleLabel?.font = .boldSystemFont(ofSize: 20)
-            self.inchonBtn.contentHorizontalAlignment = .center
         }
     }
     
@@ -59,12 +38,16 @@ class AreaSearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.areaCollectionView.dataSource = self
-        self.areaCollectionView.delegate = self
+        cityTabbarCollecionView.dataSource = self
+        cityTabbarCollecionView.delegate = self
+        areaCollectionView.dataSource = self
+        areaCollectionView.delegate = self
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let cafeBlogTableViewController = segue.destination as? CafeBlogTableViewController else { return }
+        cafeBlogTableViewController.cityName = cityNmae
+        
         if let areaInfo = sender as? Tag {
             cafeBlogTableViewController.areaName = areaInfo.tagName
         }
@@ -81,23 +64,100 @@ class AreaSearchViewController: UIViewController {
 // MARK: - UICollectionViewDataSource
 extension AreaSearchViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return areaViewModel.countAreaList
+        if collectionView == cityTabbarCollecionView {
+            return cityTabbarViewModel.countCityList
+        }
+        else {
+            if checkCityHashTable["seoul"] == 1 {
+                return seoulAreaViewModel.countSeoulAreaList
+            }
+            else if checkCityHashTable["gyeonggi"] == 1 {
+                return gyeonggiAreaViewModel.countGyeonggiAreaList
+            }
+            else {
+                return incheonAreaViewModel.countIncheonAreaList
+            }
+        }
+
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "areaCollectionViewCell", for: indexPath) as? AreaCollectionViewCell else {
-            return UICollectionViewCell()
+        if collectionView == cityTabbarCollecionView {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cityTabbarCollecionViewCell", for: indexPath) as? CityTabbarCollecionViewCell else {
+                return UICollectionViewCell()
+            }
+            let cityInfo = cityTabbarViewModel.cityInfo(at: indexPath.item)
+            cell.update(cityInfo: cityInfo)
+            return cell
         }
-        let areaInfo = areaViewModel.areaInfo(at: indexPath.item)
-        cell.update(areaInfo: areaInfo)
-        return cell
+        else {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "areaCollectionViewCell", for: indexPath) as? AreaCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            if checkCityHashTable["seoul"] == 1 {
+                let areaInfo = seoulAreaViewModel.seoulAreaInfo(at: indexPath.item)
+                cell.update(areaInfo: areaInfo)
+                return cell
+            }
+            else if checkCityHashTable["gyeonggi"] == 1 {
+                let areaInfo = gyeonggiAreaViewModel.gyeonggiAreaInfo(at: indexPath.item)
+                cell.update(areaInfo: areaInfo)
+                return cell
+            }
+            else {
+                let areaInfo = incheonAreaViewModel.incheonAreaInfo(at: indexPath.item)
+                cell.update(areaInfo: areaInfo)
+                return cell
+            }
+        }
     }
 }
 
 // MARK: - UICollectionViewDelegate
 extension AreaSearchViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let areaInfo = areaViewModel.areaInfo(at: indexPath.item)
-        performSegue(withIdentifier: "areaSearchVCToCafeBlogTableVC", sender: areaInfo)
+        if collectionView == cityTabbarCollecionView {
+            var cacheCheckCityHashTable = checkCityHashTable
+            let cityInfo = cityTabbarViewModel.cityInfo(at : indexPath.item)
+            
+            if cityInfo.tagId == "seoul" {
+                cacheCheckCityHashTable["seoul"] = 1
+                cacheCheckCityHashTable["gyeonggi"] = 0
+                cacheCheckCityHashTable["incheon"] = 0
+            }
+            else if cityInfo.tagId == "gyeonggi" {
+                cacheCheckCityHashTable["seoul"] = 0
+                cacheCheckCityHashTable["gyeonggi"] = 1
+                cacheCheckCityHashTable["incheon"] = 0
+            }
+            else {
+                cacheCheckCityHashTable["seoul"] = 0
+                cacheCheckCityHashTable["gyeonggi"] = 0
+                cacheCheckCityHashTable["incheon"] = 1
+            }
+            
+            if cacheCheckCityHashTable != checkCityHashTable {
+                checkCityHashTable = cacheCheckCityHashTable
+                areaCollectionView.reloadData()
+            }
+        }
+        
+        else {
+            if checkCityHashTable["seoul"] == 1 {
+                cityNmae = "서울"
+                let areaInfo = seoulAreaViewModel.seoulAreaInfo(at: indexPath.item)
+                performSegue(withIdentifier: "areaSearchVCToCafeBlogTableVC", sender: areaInfo)
+            }
+            else if checkCityHashTable["gyeonggi"] == 1 {
+                cityNmae = "경기도"
+                let areaInfo = gyeonggiAreaViewModel.gyeonggiAreaInfo(at: indexPath.item)
+                performSegue(withIdentifier: "areaSearchVCToCafeBlogTableVC", sender: areaInfo)
+            }
+            else {
+                cityNmae = "인천"
+                let areaInfo = incheonAreaViewModel.incheonAreaInfo(at: indexPath.item)
+                performSegue(withIdentifier: "areaSearchVCToCafeBlogTableVC", sender: areaInfo)
+            }
+        }
     }
 }
